0b6341e2962f244737c5079fcbd68b4e845672c1
Matthieu Muffato <muffato@ebi.ac.uk>
2017-11-09 13:59:06 +0000

    Added a w3_shell command

--- bin/do_w3.pl
+++ bin/do_w3.pl
@@ -7,16 +7,25 @@ use Cwd;
 use File::Basename;
 use Sys::Hostname;
 
+sub run_ssh_command {
+    my $hostname = hostname;
+    my $username = getpwuid( $< );
+    $username =~ s/ens_adm/w3_ens/;
+    print join(' ', 'ssh', $username.'@'.$hostname, @_), "\n";
+    exec('ssh', '-F', '/dev/null', $username.'@'.$hostname, @_);
+}
+
 if ($0 =~ /w3_([^\/]*)$/) {
     my $command = $1;
+    if ($command eq 'shell') {
+        run_ssh_command(@ARGV);
+        exit;
+    }
     my $d = cwd();
     if ($d =~ /\/ens_adm30\/workspace\/src\/([^\/]*)/) {
         my $dev_name = $1;
-        my $hostname = hostname;
-        my $username = getpwuid( $< );
-        $username =~ s/ens_adm/w3_ens/;
-        print join(' ', 'ssh', $username.'@'.$hostname, 'workspace/src/'.$dev_name.'/ensembl-webcode/ctrl_scripts/'.$command, @ARGV), "\n";
-        exec('ssh', $username.'@'.$hostname, 'workspace/src/'.$dev_name.'/ensembl-webcode/ctrl_scripts/'.$command, @ARGV);
+        my $script = 'workspace/src/'.$dev_name.'/ensembl-webcode/ctrl_scripts/'.$command;
+        run_ssh_command($script, @ARGV);
     } else {
         die "Cannot recognize the path '$d'\n";
     }
