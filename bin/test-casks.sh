63e8e69891e2ab1e34823ca8489c4385878147ad
Matthieu Muffato <muffato@ebi.ac.uk>
2019-07-19 15:08:30 +0100

    Use $CVS_MATT

--- bin/test-casks.sh
+++ bin/test-casks.sh
@@ -5,7 +5,7 @@ set -e
 mounts=''
 for d in cask ensembl external moonshine web
 do
-    mounts="$mounts -v $HOME/workspace/src/brew/homebrew-$d:/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/ensembl/homebrew-$d"
+    mounts="$mounts -v $CVS_MATT/brew/homebrew-$d:/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/ensembl/homebrew-$d"
 done
 exec docker run -it $mounts muffato/ensembl-linuxbrew-basic-dependencies
 
