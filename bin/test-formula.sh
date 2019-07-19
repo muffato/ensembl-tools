1135fb66c132c0659579c3bd55f0a69702b8d9e0
Matthieu Muffato <muffato@ebi.ac.uk>
2019-07-19 15:09:25 +0100

    Easier to read

--- bin/test-formula.sh
+++ bin/test-formula.sh
@@ -6,7 +6,11 @@ tempdir=$(mktemp -d)
 filename=$(basename "$1")
 cp -a "$1" "$tempdir"
 
-docker run -it -v "${tempdir}:${tempdir}" --env HOMEBREW_NO_AUTO_UPDATE=1 muffato/ensembl-linuxbrew-basic-dependencies brew install --build-from-source "$tempdir/$filename"
+docker run -it \
+    -v "${tempdir}:${tempdir}" \
+    --env HOMEBREW_NO_AUTO_UPDATE=1 \
+    muffato/ensembl-linuxbrew-basic-dependencies \
+    brew install --build-from-source "$tempdir/$filename"
 
 rm -rf "$tempdir"
 
