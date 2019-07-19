91675446871a6f804505d0a6566923b0f90c7a64
Matthieu Muffato <muffato@ebi.ac.uk>
2019-07-19 15:17:34 +0100

    Added the Moonshine archive

--- bin/test-casks.sh
+++ bin/test-casks.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-mounts=''
+MOONSHINE_NAME=ENSEMBL_MOONSHINE_ARCHIVE
+MOONSHINE_PATH_LOCAL=$CVS_MATT/brew/$MOONSHINE_NAME
+MOONSHINE_PATH_DOCKER=/home/linuxbrew/$MOONSHINE_NAME
+mounts="-v $MOONSHINE_PATH_LOCAL:$MOONSHINE_PATH_DOCKER"
+
 for d in cask ensembl external moonshine web
 do
     mounts="$mounts -v $CVS_MATT/brew/homebrew-$d:/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/ensembl/homebrew-$d"
--- bin/test-formula.sh
+++ bin/test-formula.sh
@@ -6,7 +6,12 @@ tempdir=$(mktemp -d)
 filename=$(basename "$1")
 cp -a "$1" "$tempdir"
 
+MOONSHINE_NAME=ENSEMBL_MOONSHINE_ARCHIVE
+MOONSHINE_PATH_LOCAL=$CVS_MATT/brew/$MOONSHINE_NAME
+MOONSHINE_PATH_DOCKER=/home/linuxbrew/$MOONSHINE_NAME
+
 docker run -it \
+    -v "$MOONSHINE_PATH_LOCAL:$MOONSHINE_PATH_DOCKER" \
     -v "${tempdir}:${tempdir}" \
     --env HOMEBREW_NO_AUTO_UPDATE=1 \
     muffato/ensembl-linuxbrew-basic-dependencies \
