b317981ada8545ecea1fa6a5807fb886a81f09c4
Matthieu Muffato <muffato@ebi.ac.uk>
2019-10-15 12:15:00 +0100

    Abort if a command fail

--- bin/retire-release.sh
+++ bin/retire-release.sh
@@ -1,8 +1,9 @@
 #! /bin/bash -i
 
+set -e
+
 mv "rel$1" legacy
 
-(
 	cd "legacy/rel$1"
         #rm -f *.srl
 	# We assume that ensembl-analysis is now out of reach
@@ -12,5 +13,4 @@ mv "rel$1" legacy
 		ln -sf ../../ensembl/$i
 	done
 	rm -f ensembl-webcode/conf/Plugins.pm
-)
 
