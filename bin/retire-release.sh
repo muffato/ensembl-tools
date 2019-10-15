011bf0185d3631a83fae40ece7992c2d8a7a4133
Matthieu Muffato <muffato@ebi.ac.uk>
2019-10-15 12:16:18 +0100

    Allow rerunning the script

--- bin/retire-release.sh
+++ bin/retire-release.sh
@@ -6,6 +6,7 @@ mv "rel$1" legacy
 
 	cd "legacy/rel$1"
 	rm -f reg*staging*.srl reg*mirror*srl vertannot.srl
+	rm -f ensembl-analysis ensembl-taxonomy ensembl-vep
 	# We assume that ensembl-analysis is now out of reach
 	rm -rf -- */.git
 	for i in ensembl-analysis ensembl-taxonomy ensembl-vep
