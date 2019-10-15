fb31b22b9bab39da9c94cec7052cb716bceb4d9c
Matthieu Muffato <muffato@ebi.ac.uk>
2019-10-15 12:15:51 +0100

    remove the registry files of the staging servers

--- bin/retire-release.sh
+++ bin/retire-release.sh
@@ -5,7 +5,7 @@ set -e
 mv "rel$1" legacy
 
 	cd "legacy/rel$1"
-        #rm -f *.srl
+	rm -f reg*staging*.srl reg*mirror*srl vertannot.srl
 	# We assume that ensembl-analysis is now out of reach
 	rm -rf -- */.git
 	for i in ensembl-analysis ensembl-taxonomy
