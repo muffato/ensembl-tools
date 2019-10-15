800d4b77ff5d095313273635f6ccf2d3a5f2c07d
Matthieu Muffato <muffato@ebi.ac.uk>
2019-10-15 12:14:16 +0100

    bugfix: show the whole command in order to find the elements we need

--- bin/mysql_kill_hive_job_queries.sh
+++ bin/mysql_kill_hive_job_queries.sh
@@ -4,6 +4,6 @@
 
 set -euo pipefail
 
-"$1" -N -e 'SHOW PROCESSLIST' | awk 'BEGIN{FS="\t"} $5!="Killed" && $4=="'"$2"'" && $8 ~ /^UPDATE job.*analysis_id='"'$3'"'.*READY.*CLAIMED/ {print "KILL ", $1, ";"}' | awk 'NR%2' # | "$1"
+"$1" -N -e 'SHOW FULL PROCESSLIST' | awk 'BEGIN{FS="\t"} $5!="Killed" && $4=="'"$2"'" && $8 ~ /^UPDATE job.*analysis_id='"'$3'"'.*READY.*CLAIMED/ {print "KILL ", $1, ";"}' | awk 'NR%2' # | "$1"
 #"$1" -N -e 'SHOW PROCESSLIST' | awk 'BEGIN{FS="\t"} $4=="'"$2"'" && $5 == "Searching rows for update" && $6 > 100 && $7 ~ /^UPDATE job SET worker_id=.*, status=.* WHERE analysis_id='"'$3'"'/ {print "KILL ", $1, ";"}' # | "$1"
 
