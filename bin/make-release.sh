23b33632e7c62e5ece680398ad97a6ccc0c20abd
Matthieu Muffato <muffato@ebi.ac.uk>
2019-12-12 11:46:51 +0000

    Let's copy everything since the dependencies are growing

--- bin/make-release.sh
+++ bin/make-release.sh
@@ -1,12 +1,14 @@
 #! /bin/bash -i
 
 release_number=$1
-release_dir=rel$release_number
+release_dir="${CVS_MATT}/rel$release_number"
 
-[[ -e "$release_dir" ]] || cp -a "${CVS_MATT}/.empty_release/" "$release_dir"
+mkdir -p "$release_dir"
 
-for repo in ensembl ensembl-compara ensembl-funcgen ensembl-variation ensembl-io ensembl-metadata ensembl-test ensj-healthcheck ensembl-production ensembl-rest ensembl-webcode public-plugins ensembl-tools ensembl-orm
+for repo in $(/bin/ls "${CVS_MATT}/ensembl")
 do
+    if [[ -d "${CVS_MATT}/ensembl/$repo/.git" ]]
+    then
 	if [[ -h "$release_dir/$repo" ]]
 	then
 		rm "$release_dir/$repo"
@@ -14,15 +16,16 @@ do
 
 	if [[ -e "$release_dir/$repo" ]]
 	then
-		echo "$release_dir/$repo already exists"
+		echo "$repo already exists"
 	elif [[ -e "${CVS_MATT}/ensembl/$repo/.git/refs/remotes/ensembl-github/release/$release_number" ]]
 	then
 		hecho "$repo"
 		copy_git_checkout "${CVS_MATT}/ensembl/$repo" "$release_dir/$repo" "release/$release_number"
 	else
-		hecho "$release_dir/$repo has no release/$release_number branch"
+		hecho "$repo has no release/$release_number branch"
 		ln -s "../ensembl/$repo" "$release_dir/$repo"
 	fi
+    fi
 done
 
 [ -d "$release_dir/ensembl-webcode/conf" ] && (cd "$release_dir/ensembl-webcode/conf"; ln -sf ../../../ensembl/ensembl-webcode-sandbox/Plugins.pm)
