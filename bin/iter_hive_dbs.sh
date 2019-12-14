4885f93864ecd1074714f29ce0716fbb8f9f3253
Matthieu Muffato <muffato@ebi.ac.uk>
2019-12-14 14:46:30 +0000

    Added prod-9 and prod-10

--- bin/iter_hive_dbs.sh
+++ bin/iter_hive_dbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for j in mysql-ens-compara-prod-1 mysql-ens-compara-prod-2 mysql-ens-compara-prod-3 mysql-ens-compara-prod-4 mysql-ens-compara-prod-5 mysql-ens-compara-prod-6 mysql-ens-compara-prod-7 mysql-ens-compara-prod-8
+for j in mysql-ens-compara-prod-1 mysql-ens-compara-prod-2 mysql-ens-compara-prod-3 mysql-ens-compara-prod-4 mysql-ens-compara-prod-5 mysql-ens-compara-prod-6 mysql-ens-compara-prod-7 mysql-ens-compara-prod-8 mysql-ens-compara-prod-9 mysql-ens-compara-prod-10
 do
     for i in $("$j" -N <<< 'SHOW DATABASES')
     do
