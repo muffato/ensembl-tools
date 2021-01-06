ae5e9897867f875951c61fd05e2d4f116b4effce
Matthieu Muffato <muffato@ebi.ac.uk>
2021-01-06 09:57:18 +0000

    In reality, I was more comfortable running the command myself in the container

--- bin/test-formula.sh
+++ bin/test-formula.sh
@@ -10,12 +10,13 @@ MOONSHINE_NAME=ENSEMBL_MOONSHINE_ARCHIVE
 MOONSHINE_PATH_LOCAL=$CVS_MATT/brew/$MOONSHINE_NAME
 MOONSHINE_PATH_DOCKER=/home/linuxbrew/$MOONSHINE_NAME
 
+echo Run this inside the container: brew install --build-from-source "$tempdir/$filename"
 docker run -it \
     -v "$MOONSHINE_PATH_LOCAL:$MOONSHINE_PATH_DOCKER" \
     -v "${tempdir}:${tempdir}" \
     --env HOMEBREW_NO_AUTO_UPDATE=1 \
-    muffato/ensembl-linuxbrew-basic-dependencies \
-    brew install --build-from-source "$tempdir/$filename"
+    muffato/ensembl-linuxbrew-basic-dependencies
+    #brew install --build-from-source "$tempdir/$filename"
 
 rm -rf "$tempdir"
 
